2dbd6ceb120521cc97f5888505fdcebb6627f416
Kaylea Britton <kbbritton217@gmail.com>
2017-08-22 15:14:20 -0400

    Added updating list to capitalize box

--- jsDOM/main.js
+++ jsDOM/main.js
@@ -1,7 +1,22 @@
+// Defining variables
 let input = document.getElementById("input");
 let output = document.getElementById("output");
+let listHeading = document.getElementById("listHeading");
 //console.log(input.value);
+function clear(){
+	document.getElementById('input').value="";
+}
 
+//function to show or hide list header
+function header() {
+	if (output.innerHTML === ""){
+	listHeading.innerHTML ="";
+} else {
+	listHeading.innerHTML ="<h3> Capitalized names: </h3>";
+}
+}
+
+//function to capitalize first letter of input name
 function capitalize() {
 	let newName = "";
 
@@ -9,9 +24,29 @@ function capitalize() {
 		if (l == 0) {
 			newName = input.value[l].toUpperCase();
 		} else {
-			newName += input.value[l].toLowerCase();
-		}
+			newName += input.value[l].toLowerCase(); 
+		} 
 	}
-
-	output.innerHTML = 'Capitalized name: ' + newName;
+	output.innerHTML += '<li>' + newName + '</li>';
+		header();
+		clear();
 }
+
+// jQuery Solution to using enter key
+$('input').keyup(function (e) {
+	if (e.keyCode === 13) {
+		capitalize();
+	}
+})
+
+
+
+// Javascript Solution to using enter key
+/*document.onkeypress = function (e) {
+	if (e.keyCode === 13) {
+		capitalize();
+	}
+}*/
+
+
+
